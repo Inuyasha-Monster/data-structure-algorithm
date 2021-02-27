77b1209f99f5457791f85f6da15f4e630429369d
djl394922860 <394922860@qq.com>
2021-02-27 16:06:03 +0800

    测试guava关于布隆过滤器的误判率以及基于计数器的计数器的布隆过滤器支持remove操作

---
+++ src/main/java/guava/BloomTest.java
@@ -0,0 +1,58 @@
+package guava;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author djl
+ * @create 2021/2/27 15:55
+ */
+public class BloomTest {
+
+    private static final int expectedInsertions = 1000000;
+
+    public static void main(String[] args) {
+        // 创建布隆过滤器
+        BloomFilter<String> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8), expectedInsertions);
+
+        List<String> list = new ArrayList<>(expectedInsertions);
+
+        for (int i = 0; i < expectedInsertions; i++) {
+            String uuid = UUID.randomUUID().toString();
+            bloomFilter.put(uuid);
+            list.add(uuid);
+        }
+
+        int mightContainNum1 = 0;
+
+        NumberFormat percentFormat = NumberFormat.getPercentInstance();
+        percentFormat.setMaximumFractionDigits(2); //最大小数位数
+
+        for (int i = 0; i < 500; i++) {
+            String key = list.get(i);
+            if (bloomFilter.mightContain(key)) {
+                mightContainNum1++;
+            }
+        }
+        System.out.println("【key真实存在的情况】布隆过滤器认为存在的key值数：" + mightContainNum1);
+        System.out.println("-----------------------分割线---------------------------------");
+
+        int mightContainNum2 = 0;
+
+        for (int i = 0; i < expectedInsertions; i++) {
+            String key = UUID.randomUUID().toString();
+            if (bloomFilter.mightContain(key)) {
+                mightContainNum2++;
+            }
+        }
+
+        System.out.println("【key不存在的情况】布隆过滤器认为存在的key值数：" + mightContainNum2);
+        System.out.println("【key不存在的情况】布隆过滤器的误判率为：" + percentFormat.format((float) mightContainNum2 / expectedInsertions));
+    }
+}
