ca74cded5408c0dd7b3741a4b9b89603453769d5
djl394922860 <394922860@qq.com>
2021-02-28 11:08:22 +0800

    算法题:寻找窗口最大值 https://leetcode-cn.com/problems/sliding-window-maximum/

--- src/main/java/window/LeetCode239.java
+++ src/main/java/window/LeetCode239.java
@@ -1,5 +1,6 @@
 package window;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -12,10 +13,22 @@ public class LeetCode239 {
 
     public static void main(String[] args) {
         LeetCode239 leetCode239 = new LeetCode239();
-        int[] arr = new int[]{9, 10, 9, -7, -4, -8, 2, -6};
-        int[] result = leetCode239.maxSlidingWindow(arr, 5);
+
+        int[] arr = new int[]{6, 3, -1, -3, 5, 3, 6, 7};
+        int[] result = leetCode239.maxSlidingWindow(arr, 3);
         for (int i : result) {
             System.out.print(i + " ");
+        }
+        System.out.println("采用优先级队列解答:");
+        int[] window2 = leetCode239.maxSlidingWindow2(arr, 3);
+        for (int i : window2) {
+            System.out.println(i + " ");
+        }
+
+        System.out.println("采用自用优先级队列");
+        int[] result2 = leetCode239.maxSlidingWindow3(arr, 3);
+        for (int i : result2) {
+            System.out.println(i + " ");
         }
     }
 
@@ -74,13 +87,56 @@ public class LeetCode239 {
      * @param k
      * @return
      */
-//    public int[] maxSlidingWindow2(int[] nums, int k) {
-//        int[] result = new int[nums.length - k + 1];
-//
-//        PriorityQueue
-//
-//        for (int i = 0; i < nums.length - k + 1; i++) {
-//
-//        }
-//    }
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+
+    public int[] maxSlidingWindow3(int[] nums, int k) {
+        // 定义优先级队列
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] pair1, int[] pair2) {
+                // 比较每个元素的值,如果不相等直接比较值,如果值相等则比较索引位置
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        // 初始化k窗口的优先级队列
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        // 滑动窗口找到最大值
+        int[] result = new int[nums.length - k + 1];
+        // 赋值第一个窗口的最大值
+        result[0] = pq.peek()[0];
+        // 开始滑动
+        for (int i = k; i < nums.length; ++i) {
+            // 将新滑动的值以及索引加入优先级队列
+            pq.offer(new int[]{nums[i], i});
+            // 二叉堆的最大元素的索引小于等于当前索引则移除 ( 判断当前堆顶元素是否在当前的滑动窗口中 )
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            result[i - k + 1] = pq.peek()[0];
+        }
+        return result;
+    }
 }
