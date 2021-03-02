7b4103a96bbe0b12988a496dd5d3d5688eadb569
djlnet <djlnet@17track.cn>
2021-03-02 16:01:18 +0800

    通过信号量abc还控制abc线程交替打印打印

--- src/main/java/thrad/SemaphoreABCTest.java
+++ src/main/java/thrad/SemaphoreABCTest.java
@@ -1,5 +1,6 @@
 package thrad;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,50 @@ import java.util.concurrent.locks.ReentrantLock;
  * 答案:通过三个信号量来解决
  */
 public class SemaphoreABCTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphoreA = new Semaphore(1);
+        Semaphore semaphoreB = new Semaphore(0);
+        Semaphore semaphoreC = new Semaphore(0);
 
+        Thread threadA = new Thread(() -> {
+            while (true) {
+                try {
+                    semaphoreA.acquire();
+                    System.out.print("A");
+                    semaphoreB.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            while (true) {
+                try {
+                    semaphoreB.acquire();
+                    System.out.print("B");
+                    semaphoreC.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            while (true) {
+                try {
+                    semaphoreC.acquire();
+                    System.out.print("C");
+                    System.out.println();
+                    semaphoreA.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
     }
 }
