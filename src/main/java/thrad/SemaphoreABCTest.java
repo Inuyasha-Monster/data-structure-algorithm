1b7db778a59fcabb5fe88c7aa41156524b42e0d6
djlnet <djlnet@17track.cn>
2021-03-02 14:17:07 +0800

    编写面试题

---
+++ src/main/java/thrad/SemaphoreABCTest.java
@@ -0,0 +1,17 @@
+package thrad;
+
+import java.sql.Timestamp;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author djl
+ * @create 2021/3/2 11:32
+ * ABC三个线程交替打印ABCABC...
+ */
+public class SemaphoreABCTest {
+    public static void main(String[] args) {
+
+    }
+}
