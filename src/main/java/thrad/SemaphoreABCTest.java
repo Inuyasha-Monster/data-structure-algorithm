793d8a581d3b04070865ee8910ebb316141029be
djlnet <djlnet@17track.cn>
2021-03-02 15:38:11 +0800

    编写面试题

--- src/main/java/thrad/SemaphoreABCTest.java
+++ src/main/java/thrad/SemaphoreABCTest.java
@@ -8,7 +8,8 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author djl
  * @create 2021/3/2 11:32
- * ABC三个线程交替打印ABCABC...
+ * 题目:ABC三个线程交替打印ABCABC...
+ * 答案:通过三个信号量来解决
  */
 public class SemaphoreABCTest {
     public static void main(String[] args) {
